81455cba676230201265840cb2e86c93f4763e49
mcuong <huynhmc2804@gmail.com>
2025-04-05 23:38:21 +0700

    xong quá là ok hehe boi

--- stock_management/stockb/urls.py
+++ stock_management/stockb/urls.py
@@ -3,7 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('stock-out/', views.stock_out),
+    path('stock-out/', views.stock_out, name='stock_out'),
+    path('stock-out/create', views.stock_out_update, name='stock_out_create'),
+
+
+
+    path('stock-in/', views.stock_in, name='stock_in'),
+    path('stock-in/create', views.stock_in_update, name='stock_in_create'),
 
 
 ]
--- stock_management/stockb/views.py
+++ stock_management/stockb/views.py
@@ -5,4 +5,24 @@ def index(request):
     return render(request, "main.html")
 
 def stock_out(request):
-    return render(request, "stock_out/stock_out.html")
+    context = {"title": "Trang xuất kho"}
+    return render(request, "stock_out/stock_out.html", context)
+
+def stock_out_update(request):
+    context = {"title": "Trang tạo mới đơn xuất kho"}
+    return render(request, "stock_out/stock_out_update.html", context)
+
+
+
+
+
+
+
+
+
+def stock_in(request):
+    context = {"title": "Trang nhập kho"}
+    return render(request, "stock_in/stock_in.html", context )
+def stock_in_update(request):
+    context = {"title": "Trang tạo mới đơn nhập kho"}
+    return render(request, "stock_in/stock_in_update.html", context)
