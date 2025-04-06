18e1d63dbcc7f26f7d30f7a30ebbbc14701f204c
huynhbao123 <baobbbb2@gmail.com>
2025-04-06 16:32:56 +0700

    Cập nhật giao diện low_stock_list, near_expiry_list, supplier và các thay đổi trong views, urls, base.html; thêm .gitignore

--- stock_management/stockb/urls.py
+++ stock_management/stockb/urls.py
@@ -3,10 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('supplier', views.supplier_list_view, name='supplier_list'),
+    path('supplier/create', views.supplier_create_view, name='supplier_create'),  # URL cho trang tạo nhà cung cấp
+    path('inventory/near-expiry', views.near_expiry_list_view, name='near_expiry_list'),  # URL cho danh sách sản phẩm sắp hết hạn
+    path('inventory/low-stock', views.low_stock_list_view, name='low_stock_list'),  # URL cho danh sách sản phẩm gần hết trong kho
     path('stock-out/', views.stock_out, name='stock_out'),
     path('stock-out/create', views.stock_out_update, name='stock_out_create'),
-
-
 
     path('stock-in/', views.stock_in, name='stock_in'),
     path('stock-in/create', views.stock_in_update, name='stock_in_create'),
--- stock_management/stockb/views.py
+++ stock_management/stockb/views.py
@@ -12,17 +12,21 @@ def stock_out_update(request):
     context = {"title": "Trang tạo mới đơn xuất kho"}
     return render(request, "stock_out/stock_out_update.html", context)
 
+# View cho thông tin nhà cung cấp
+def supplier_list_view(request):
+    return render(request, 'supplier/supplier_list.html')
 
+# View cho tạo mới nhà cung cấp
+def supplier_create_view(request):
+    if request.method == 'POST':
+        return redirect('supplier_list')  # Quay lại danh sách nhà cung cấp sau khi lưu
+    return render(request, 'supplier/supplier_create.html')
 
+# View mới cho danh sách sản phẩm sắp hết hạn
+def near_expiry_list_view(request):
+    return render(request, 'inventory/near_expiry_list.html')
 
+# View mới cho danh sách sản phẩm gần hết trong kho
+def low_stock_list_view(request):
+    return render(request, 'inventory/low_stock_list.html')
 
-
-
-
-
-def stock_in(request):
-    context = {"title": "Trang nhập kho"}
-    return render(request, "stock_in/stock_in.html", context )
-def stock_in_update(request):
-    context = {"title": "Trang tạo mới đơn nhập kho"}
-    return render(request, "stock_in/stock_in_update.html", context)
