4759764d3a0ee2c6047282af1caa0af474aa46f1
huynhbao123 <baobbbb2@gmail.com>
2025-04-06 16:32:56 +0700

    Cập nhật giao diện low_stock_list, near_expiry_list, supplier và các thay đổi trong views, urls, base.html; thêm .gitignore

--- stock_management/stockb/urls.py
+++ stock_management/stockb/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('stock-out/', views.stock_out),
+    path('supplier', views.supplier_list_view, name='supplier_list'),
+    path('supplier/create', views.supplier_create_view, name='supplier_create'),  # URL cho trang tạo nhà cung cấp
+    path('inventory/near-expiry', views.near_expiry_list_view, name='near_expiry_list'),  # URL cho danh sách sản phẩm sắp hết hạn
+    path('inventory/low-stock', views.low_stock_list_view, name='low_stock_list'),  # URL cho danh sách sản phẩm gần hết trong kho
 
-
-]
+    ]
--- stock_management/stockb/views.py
+++ stock_management/stockb/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def index(request):
@@ -6,3 +7,21 @@ def index(request):
 
 def stock_out(request):
     return render(request, "stock_out/stock_out.html")
+
+# View cho thông tin nhà cung cấp
+def supplier_list_view(request):
+    return render(request, 'supplier/supplier_list.html')
+
+# View cho tạo mới nhà cung cấp
+def supplier_create_view(request):
+    if request.method == 'POST':
+        return redirect('supplier_list')  # Quay lại danh sách nhà cung cấp sau khi lưu
+    return render(request, 'supplier/supplier_create.html')
+
+# View mới cho danh sách sản phẩm sắp hết hạn
+def near_expiry_list_view(request):
+    return render(request, 'inventory/near_expiry_list.html')
+
+# View mới cho danh sách sản phẩm gần hết trong kho
+def low_stock_list_view(request):
+    return render(request, 'inventory/low_stock_list.html')
