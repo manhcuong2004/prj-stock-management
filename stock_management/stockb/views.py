4e1b50a3c78a629d556a165c2d372d85c031adae
mcuong <huynhmc2804@gmail.com>
2025-04-07 20:12:41 +0700

    sua chart

--- stock_management/stockb/views.py
+++ stock_management/stockb/views.py
@@ -7,7 +7,6 @@ def index(request):
     return render(request, "main.html")
 
 def stock_out(request):
-    return render(request, "stock_out/stock_out.html")
     context = {"title": "Trang xuất kho"}
     return render(request, "stock_out/stock_out.html", context)
 
@@ -25,34 +24,45 @@ def stock_in_update(request):
 
 
 def supplier_list_view(request):
-    return render(request, 'supplier/supplier_list.html')
+    context = {"title": "Danh sách nhà cung cấp"}
+    return render(request, 'supplier/supplier_list.html', context)
 
 def supplier_create_view(request):
+    context = {"title": "Tạo mới nhà cung cấp"}
     if request.method == 'POST':
         return redirect('supplier_list')
-    return render(request, 'supplier/supplier_create.html')
+    return render(request, 'supplier/supplier_create.html',context)
 
 def near_expiry_list_view(request):
-    return render(request, 'inventory/near_expiry_list.html')
+    context = {"title": "Hàng gần đến ngày khuyến nghị"}
+    return render(request, 'inventory/near_expiry_list.html', context)
 
 def low_stock_list_view(request):
-    return render(request, 'inventory/low_stock_list.html')
+    context = {"title": "Hàng gần hết trong kho"}
+    return render(request, 'inventory/low_stock_list.html',context)
 
 
 def units_view(request):
-    return render(request, 'units/units.html')
+    context = {"title": "Danh sách đơn vị"}
+    return render(request, 'units/units.html', context)
 def create_unit(request):
-    return render(request, 'units/create_unit.html')
+    context = {"title": "Tạo mới đơn vị"}
+    return render(request, 'units/create_unit.html',context)
 def report_overview(request):
-    return render(request, 'report/overview.html')
+    context = {"title": "Báo cáo"}
+    return render(request, 'report/overview.html', context)
 
 
 def product_category_view(request):
-    return render(request, 'product_category/product_category_list.html')
+    context = {"title": "Danh mục sản phẩm"}
+    return render(request, 'product_category/product_category_list.html', context)
 def product_category_create(request):
-    return render(request, 'product_category/product_category_update.html')
+    context = {"title": "Tạo mới danh mục sản phẩm"}
+    return render(request, 'product_category/product_category_update.html', context)
 def product_category_detail(request):
-    return render(request, 'product_category/product_category_detail.html')
+    context = {"title": "Chi tiết sản phẩm"}
+    return render(request, 'product_category/product_category_detail.html', context)
 
 def product_view(request):
-    return render(request, 'product/product_list.html')
+    context = {"title": "Danh sách sản phẩm"}
+    return render(request, 'product/product_list.html', context)
