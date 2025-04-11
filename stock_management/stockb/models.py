6957ebc6f5564a7590614f6c32b1f419b1b26e8b
mcuong <huynhmc2804@gmail.com>
2025-04-11 21:30:08 +0700

    sua chart

--- stock_management/stockb/models.py
+++ stock_management/stockb/models.py
@@ -3,7 +3,10 @@ from django.db import models
 
 class ProductCategory(models.Model):
     category_name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
@@ -22,7 +25,7 @@ class Supplier(models.Model):
     address = models.TextField()
     phone = models.CharField(max_length=20)
     email = models.EmailField()
-    notes = models.TextField(blank=True)
+    notes = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
@@ -33,7 +36,7 @@ class Product(models.Model):
     quantity = models.IntegerField()
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
     selling_price = models.DecimalField(max_digits=10, decimal_places=2)
-    minimum_stock = models.IntegerField()
+    minimum_stock = models.IntegerField(default=0)
     inspection_time = models.DateTimeField()
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
@@ -43,12 +46,21 @@ class Product(models.Model):
 
 
 class Employee(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Nam'),
+        ('F', 'Nữ'),
+        ('O', 'Khác'),
+    ]
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
     phone = models.CharField(max_length=20)
-    gender = models.CharField(max_length=10)
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_CHOICES,
+        default=None
+    )
     address = models.TextField()
     role = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -58,19 +70,35 @@ class Employee(models.Model):
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.EmailField()
+    email = models.EmailField(blank=True,null=True)
     phone = models.CharField(max_length=20)
     address = models.TextField()
-    status = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
 
 class StockIn(models.Model):
+    PAYMENT_STATUS_CHOICES = [
+        ('UNPAID', 'Chưa thanh toán'),
+        ('PAID', 'Đã thanh toán'),
+    ]
+    IMPORT_STATUS_CHOICES = [
+        ('IN_PROGRESS', 'Đang xử lí'),
+        ('COMPLETED', 'Completed'),
+        ('CANCELLED', 'Cancelled'),
+    ]
     import_date = models.DateTimeField()
     total_amount = models.DecimalField(max_digits=12, decimal_places=2)
-    payment_status = models.CharField(max_length=50)
-    import_status = models.CharField(max_length=50)
+    payment_status = models.CharField(
+        max_length=20,
+        choices=PAYMENT_STATUS_CHOICES,
+        default='PENDING'
+    )
+    import_status = models.CharField(
+        max_length=20,
+        choices=IMPORT_STATUS_CHOICES,
+        default='IN_PROGRESS'
+    )
     notes = models.TextField(blank=True)
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
@@ -79,9 +107,26 @@ class StockIn(models.Model):
 
 
 class StockOut(models.Model):
+    PAYMENT_STATUS_CHOICES = [
+        ('UNPAID', 'Chưa thanh toán'),
+        ('PAID', 'Đã thanh toán'),
+    ]
+    IMPORT_STATUS_CHOICES = [
+        ('IN_PROGRESS', 'Đang xử lí'),
+        ('COMPLETED', 'Completed'),
+        ('CANCELLED', 'Cancelled'),
+    ]
     export_date = models.DateTimeField()
-    export_status = models.CharField(max_length=50)
-    payment_status = models.CharField(max_length=50)
+    payment_status = models.CharField(
+        max_length=20,
+        choices=PAYMENT_STATUS_CHOICES,
+        default='PENDING'
+    )
+    import_status = models.CharField(
+        max_length=20,
+        choices=IMPORT_STATUS_CHOICES,
+        default='IN_PROGRESS'
+    )
     notes = models.TextField(blank=True)
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
@@ -93,7 +138,7 @@ class StockInDetail(models.Model):
     import_record = models.ForeignKey(StockIn, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
-    discount = models.DecimalField(max_digits=5, decimal_places=2)
+    discount = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
     product_batch = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
@@ -103,6 +148,6 @@ class StockOutDetail(models.Model):
     export_record = models.ForeignKey(StockOut, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
-    discount = models.DecimalField(max_digits=5, decimal_places=2)
+    discount = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
